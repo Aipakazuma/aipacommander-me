b95e306169f36fd43da366787e281d1feadc0da0
aipacommander <gundamgundam777@gmail.com>
2016-01-19 20:52:39 +0900

    [add] added gulpfile and static files.

--- README.md
+++ README.md
@@ -16,6 +16,21 @@ CSSの勉強したいから立ち上げました。
 
 * gulp
 
+installしたモジュール一覧
+
+```sh
+$ npm install -g gulp
+$ npm install -D gulp
+$ npm install gulp-sass
+$ npm install gulp-imagemin
+$ npm install imagemin-pngquant
+$ npm install gulp-minify-css
+$ npm install gulp-react
+$ npm install gulp-flowtype
+$ npm install gulp-ruby-haml
+$ npm install gulp-watch
+```
+
 ## インフラ
 
 * aws s3
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var minifyCss    = require('gulp-minify-css');
+var watch        = require('gulp-watch');
+var react        = require('gulp-react');
+var flow         = require('gulp-flowtype');
+var documentRoot = './';
+var development  = 'development/';
+var publicFolder = 'public/';
+var hamlDirectory = documentRoot + development + 'haml/**/*.haml';
+var scssDirectory = documentRoot + development + 'scss/**/*.scss'
+
+// ####################
+// ####### scss #######
+// ####################
+gulp.task('sass', function() {
+  gulp.src(scssDirectory)
+    .pipe(sass())
+    .pipe(minifyCss())
+    .pipe(gulp.dest(documentRoot + publicFolder + 'css'));
+});
+
+// ####################
+// ####### haml #######
+// ####################
+var haml = require('gulp-ruby-haml');
+gulp.task('haml', function() {
+  gulp.src(documentRoot + development + 'haml/**/*.haml')
+    .pipe(haml())
+    .pipe(gulp.dest(documentRoot + publicFolder));
+});
+
+// watch
+gulp.task('watch', function() {
+  watch(scssDirectory, function() {
+    gulp.start('sass');
+  });
+  watch(hamlDirectory, function() {
+      gulp.start('haml');
+  });
+});
